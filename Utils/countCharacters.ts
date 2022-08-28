9cf9db35bc613713a92dffbc4bc7318706166cc0
Thomas Baxendale <tombaxendale95@gmail.com>
2022-08-28 17:44:18 +0100

    test utility for counting the number of command characters in each string

---
+++ Utils/countCharacters.ts
@@ -0,0 +1,7 @@
+export const countCharacters = (s: string) => {
+    console.log(s.split("F").length - 1);
+    console.log(s.split("B").length - 1);
+    console.log(s.split("R").length - 1);
+    console.log(s.split("L").length - 1);
+
+}
