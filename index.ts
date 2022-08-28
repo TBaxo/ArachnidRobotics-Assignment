8282d88bfba9370ced156c2153881cf172087a18
Thomas Baxendale <tombaxendale95@gmail.com>
2022-08-28 19:48:02 +0100

    updated run script so you can select a robot type (default is the newest)

--- index.ts
+++ index.ts
@@ -2,9 +2,20 @@ import { Point } from "./classes/Point";
 import { RobotSpiderMk1 } from "./classes/RobotSpiderMk1";
 import { countCharacters } from "./Utils/countCharacters";
 import PromptSync, {  } from "prompt-sync";
-
+import { IRobotSpider } from "./interfaces/IRobotSpider";
+import { Heading, RobotSpiderMk2 } from "./classes/RobotSpiderMk2";
 
 const prompt = PromptSync();
+
+//Default value of 2
+const getRobot = (robotVersion: number, startPosition: Point): IRobotSpider => { 
+    if (robotVersion == 1) return new RobotSpiderMk1(startPosition);
+
+    return new RobotSpiderMk2(startPosition, Heading.North);
+}
+
+const robotVersion: number = Number(prompt('What version of spider are you running (currently support 1 and 2): '));
+if (Number.isNaN(robotVersion)) throw "Invalid X co-ordinate";
 
 const startingPositionX: number = Number(prompt('What is the starting x co-ordinate?: '));
 if (Number.isNaN(startingPositionX)) throw "Invalid X co-ordinate";
@@ -14,8 +25,9 @@ if (Number.isNaN(startingPositionY)) throw "Invalid Y co-ordinate";
 
 const commands: string = prompt('Please enter your set of commands: ');
 
+
 const startPosition = new Point(startingPositionX, startingPositionY);
-const robot = new RobotSpiderMk1(startPosition);
+const robot = getRobot(robotVersion, startPosition);
 
 robot.applyCommands(commands);
 
